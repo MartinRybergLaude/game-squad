4f4b433b018e25c241ba82d6022210c67171d726
Martin Ryberg Laude <martin.ryberg.laude@gmail.com>
2022-11-28 16:18:57 +0100

    feat: add routing solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import { MantineProvider, Text } from "@mantine/core";
+import {
+  createReactRouter,
+  createRouteConfig,
+  RouterProvider,
+} from "@tanstack/react-router";
+
+const rootRoute = createRouteConfig();
+
+const indexRoute = rootRoute.createRoute({
+  path: "/",
+  component: () => <Text>Index</Text>,
+});
+
+const loginRoute = rootRoute.createRoute({
+  path: "/login",
+  component: () => <Text>Login</Text>,
+});
+
+const routeConfig = rootRoute.addChildren([indexRoute, loginRoute]);
+
+const router = createReactRouter({ routeConfig });
 
 function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>GameSquad</Text>
+      <RouterProvider router={router}>
+        <Text>GameSquad</Text>
+      </RouterProvider>
     </MantineProvider>
   );
 }
