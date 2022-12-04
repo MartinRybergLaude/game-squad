2b16595d55c85cf07e2d34928bb7752d6462ef74
Vilhelm Norström <vilhelm@Vilhelms-MacBook-Air.local>
2022-12-04 20:23:48 +0100

    add componets gameCollection

--- src/pages/Dashboard/dashboardView.tsx
+++ src/pages/Dashboard/dashboardView.tsx
@@ -2,11 +2,14 @@ import {
     Grid,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { BadgeCard} from "../../components/gameCollection/gameCollectionView";
 
 export default function DashboardView() {
     return <Grid>
-        <Grid.Col span={1}>
+        <Grid.Col span={4}>
             <div>Dashboard</div>
+            <BadgeCard/>
         </Grid.Col>
     </Grid>
 }
+
