324125caf4240a17a3687a18c63feb81af9b4fdb
Martin Ryberg Laude <martin.ryberg.laude@gmail.com>
2022-12-04 20:46:00 +0100

    fix: dashboard page

--- src/pages/Dashboard/dashboardView.tsx
+++ src/pages/Dashboard/dashboardView.tsx
@@ -1,48 +1,32 @@
-import { Grid } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import {
-    Grid,
-    Center,
-    createStyles,
-} from "@mantine/core";
+import { Grid, Center, createStyles } from "@mantine/core";
 import SidebarPresenter from "../../components/sidebar/presenter";
 import { NavbarSimple } from "../../components/sidebar/view";
 
 const useStyles = createStyles((theme) => ({
-    formCenter: {
-      height: "100vh",
-      [`@media (max-width: ${theme.breakpoints.md}px)`]: {
-        height: "auto",
-      },
+  formCenter: {
+    height: "100vh",
+    [`@media (max-width: ${theme.breakpoints.md}px)`]: {
+      height: "auto",
     },
+  },
 }));
 
 export default function DashboardView() {
+  const { classes } = useStyles();
+
   return (
-    <Grid>
+    <Grid grow>
       <Grid.Col span={1}>
-        <div>Dashboard</div>
+        <div>
+          <NavbarSimple />
+        </div>
+      </Grid.Col>
+      <Grid.Col span={2}>
+        <div>
+          <Center className={classes.formCenter}>Component2</Center>
+        </div>
       </Grid.Col>
     </Grid>
   );
 }
-
-    const { classes } = useStyles();
-    
-    return (
-        <Grid grow>
-            <Grid.Col span={1}>
-                <div>
-                    <NavbarSimple/>
-                </div>
-            </Grid.Col>
-            <Grid.Col span={2}>
-                <div>
-                    <Center className={classes.formCenter}>
-                        Component2
-                    </Center>
-                </div>
-            </Grid.Col>
-        </Grid>
-    );
-  }
