52e980f91d224d8b66b1ae594393964d141ef964
Sa-tec <sagaagnes@gmail.com>
2022-12-07 12:15:42 +0100

    broken fetch

--- src/components/search/searchFunction.tsx
+++ src/components/search/searchFunction.tsx
@@ -19,29 +19,32 @@ const twitchKey = fetch(
 )
   .then(response => response.json())
   .then(data => {
-    return data;
+    return JSON.parse(JSON.stringify(data)).access_token.trim();
   });
 
 console.log(await twitchKey);
 
-// .then(response => JSON.parse(response));
-
-// function getDishDetails() {
-//   // endpoint = endpoint? endpoint: "" //If endpoint undefined (falsy), endpoint = empty string
-//   // params = params? params: "" //If params undefined (falsy), params = empty string
-//   return fetch(
-//     " https://api-docs.igdb.com/#game",
-//     {
-//       // object literal
-//       method: "GET", // HTTP method
-//       headers: {
-//         'Accept': 'application/json',
-//         'Client-ID: Client ID',
-//         'Authorization: Bearer access_token',
-//       }, // end of headers object
-//     } /* end of second fetch parameter, object */,
-//   ).then(treatHTTPResponseACB);
-// }
+async function getGames() {
+  const key = await twitchKey;
+  // endpoint = endpoint? endpoint: "" //If endpoint undefined (falsy), endpoint = empty string
+  // params = params? params: "" //If params undefined (falsy), params = empty string
+  return fetch(
+    "https://api.igdb.com/v4/games",
+    {
+      // object literal
+      method: "POST",
+      // mode: "no-cors",
+      headers: {
+        Accept: "application/json",
+        "Client-ID": client_id,
+        Authorization: "Bearer " + key,
+        "Access-Control-Allow-Origin": "https://api.igdb.com/v4/games", //I have no idea what this does
+      }, // end of headers object
+      // data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;",
+      // data: "fields name; limit 10;",
+    } /* end of second fetch parameter, object */,
+  ).then(response => response.json());
+}
 
 // function treatHTTPResponseACB(response) {
 //   if (response.status == 200) {
@@ -50,6 +53,8 @@ console.log(await twitchKey);
 //     throw "Error";
 //   }
 // }
+
+console.log(getGames());
 
 // function searchDishes(params) {
 //   // Just in case an undefined slipped in
