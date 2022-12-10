f1df3e546b8d25ff88fbbd93e4adbaf6e1c214e0
Martin Ryberg Laude <martin.ryberg.laude@gmail.com>
2022-12-10 19:04:25 +0100

    fix: sidebar squad create btns

--- src/components/squadSelect/squadSelectView.tsx
+++ src/components/squadSelect/squadSelectView.tsx
@@ -1,15 +1,25 @@
-import { Button, createStyles, Title } from "@mantine/core";
+import { Button, createStyles, Group, Title } from "@mantine/core";
+import { openModal } from "@mantine/modals";
 import { useAtom } from "jotai";
 
 import { selectedSquadIdAtom, squadsAtom } from "~/store";
 import { Squad } from "~/types";
 
+import CreateSquadModalPresenter from "../createSquadModal/createSquadModalPresenter";
+
 const useStyles = createStyles(theme => ({
   btn: {
-    marginTop: theme.spacing.md,
+    marginTop: theme.spacing.sm,
+    paddingLeft: 8,
     "> div": {
       justifyContent: "flex-start",
     },
+  },
+  bottomDivider: {
+    paddingBottom: theme.spacing.md,
+    borderBottom: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2]
+    }`,
   },
 }));
 
@@ -29,9 +39,25 @@ export default function GroupSelectView() {
 
   return (
     <>
-      <Title ml={16} order={3}>
+      <Title ml={8} order={3}>
         Squads
       </Title>
+      <Group grow mt={16} className={classes.bottomDivider}>
+        <Button
+          compact
+          onClick={() => {
+            openModal({
+              title: "Create Squad",
+              children: <CreateSquadModalPresenter />,
+            });
+          }}
+        >
+          Create
+        </Button>
+        <Button variant="outline" compact>
+          Join
+        </Button>
+      </Group>
       {squads.map(squad => (
         <Button
           className={classes.btn}
