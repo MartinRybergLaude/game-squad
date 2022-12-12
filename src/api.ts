19c68f8edd28469170f40765ec03b54882a120fe
Sa-tec <sagaagnes@gmail.com>
2022-12-12 13:52:42 +0100

    Search now doesn't bring up DLCs

--- src/api.ts
+++ src/api.ts
@@ -13,8 +13,8 @@ export function getGamesByIds(ids: string[]): Promise<Game[]> {
 export function getGamesBySearch(searchText: string): Promise<Game[]> {
   return fetch(`${API_URL}`, {
     method: "POST",
-    body: `fields name, cover.url, first_release_date, platforms.abbreviation, rating, rating_count, summary, total_rating, total_rating_count, url;
-           where name ~ "${searchText}"*;
+    body: `fields name, cover.url, category, first_release_date, platforms.abbreviation, rating, rating_count, summary, total_rating, total_rating_count, url;
+           where name ~ "${searchText}"* & category = 0;
            sort rating desc;
            limit 5;`,
   }).then(response => response.json());
